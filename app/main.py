dc3200c1020d445a5d9e38135c5935f3514e9caa
Sebastian <jspenalozap@gmail.com>
2026-01-07 13:45:35 -0500

    Add CORS

--- app/main.py
+++ app/main.py
@@ -1,22 +1,23 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.api.routes.requests import router as solicitudes_router
+from fastapi.middleware.cors import CORSMiddleware # Importar esto
+from app.api.routes import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI(title="Motor de Reglas de Priorización")
 
-# ESTO DA PERMISO AL FRONTEND PARA CONECTARSE
+# --- AGREGAR ESTE BLOQUE ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"], # En producción podrías poner la URL de tu frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+# ---------------------------
 
-app.include_router(solicitudes_router)
+app.include_router(requests.router)
 
 @app.get("/health")
 def health():
