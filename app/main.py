246f16843264cce9bfad27c4b6c5682b272923f9
Sebastian <jspenalozap@gmail.com>
2026-01-07 14:03:45 -0500

    Add CORS support

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware # Importar esto
+from fastapi.middleware.cors import CORSMiddleware  # Importar esto
 from app.api.routes import requests
 from dotenv import load_dotenv
 
@@ -10,7 +10,7 @@ app = FastAPI(title="Motor de Reglas de Priorización")
 # --- AGREGAR ESTE BLOQUE ---
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # En producción podrías poner la URL de tu frontend
+    allow_origins=["*"],  # En producción podrías poner la URL de tu frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
